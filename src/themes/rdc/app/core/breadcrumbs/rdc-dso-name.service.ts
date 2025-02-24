f3a9bcfe591bc5653bf4fc388a93f61b65445c4d
William Welling <wwelling@tamu.edu>
2025-02-24 10:26:59 -0600

    Translate dataset supplement to

--- src/themes/rdc/app/core/breadcrumbs/rdc-dso-name.service.ts
+++ src/themes/rdc/app/core/breadcrumbs/rdc-dso-name.service.ts
@@ -1,13 +1,21 @@
-import { DSpaceObject } from '../../../../../app/core/shared/dspace-object.model';
 import { Injectable } from '@angular/core';
+import { DSpaceObject } from '../../../../../app/core/shared/dspace-object.model';
 
-import { isNotEmpty } from '../../../../../app/shared/empty.util';
+import { TranslateService } from '@ngx-translate/core';
 import {
   DSONameService
 } from '../../../../../app/core/breadcrumbs/dso-name.service';
+import { isNotEmpty } from '../../../../../app/shared/empty.util';
 
 @Injectable()
 export class RdcDSONameService extends DSONameService {
+
+  readonly translate: TranslateService;
+
+  constructor(translateService: TranslateService) {
+    super(translateService);
+    this.translate = translateService;
+  }
 
   getName(dso: DSpaceObject | undefined): string {
     if (this.isDataset(dso)) {
@@ -15,7 +23,9 @@ export class RdcDSONameService extends DSONameService {
       const project = dso.firstMetadataValue('dc.title.project');
 
       if (isNotEmpty(dataset) && isNotEmpty(project)) {
-        return `${dataset}: Supplement to ${project}`;
+        return this.translate.instant('dataset.supplement.to', {
+          dataset, project
+        });
       } else if (isNotEmpty(dataset)) {
         return dataset;
       }
